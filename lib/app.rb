1426a707f9fd231eba26e63312644078a7539fbd
Kelifa, Alexandre <alexandre.kelifa@hotmail.com>
2020-04-15 23:17:53 +0200

    first twit twitted

---
+++ lib/app.rb
@@ -0,0 +1,16 @@
+ # ligne très importante qui appelle la gem.
+ require 'twitter'
+
+ require 'dotenv'# Appelle la gem Dotenv
+Dotenv.load(".env")
+ 
+ # quelques lignes qui appellent les clés d'API de ton fichier .env
+ client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+ end
+ 
+ # ligne qui permet de tweeter sur ton compte
+ client.update('Hello confined world!!! Here is my first tweet. And it was made with Ruby !!!!')
